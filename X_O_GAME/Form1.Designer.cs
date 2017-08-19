4b741c49d5beca96aff1a6dc27d49570a0e11a1b
MohammedSaid1 <mohammadsaid694@gmail.com>
2017-08-19 15:13:17 +0200

    Add files via upload

---
+++ X_O_GAME/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace X_O_GAME
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn00 = new System.Windows.Forms.Button();
+            this.btn01 = new System.Windows.Forms.Button();
+            this.btn02 = new System.Windows.Forms.Button();
+            this.btn12 = new System.Windows.Forms.Button();
+            this.btn11 = new System.Windows.Forms.Button();
+            this.btn10 = new System.Windows.Forms.Button();
+            this.btn22 = new System.Windows.Forms.Button();
+            this.btn21 = new System.Windows.Forms.Button();
+            this.btn20 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn00
+            // 
+            this.btn00.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn00.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn00.FlatAppearance.BorderSize = 0;
+            this.btn00.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn00.Font = new System.Drawing.Font("Arial", 40F, System.Drawing.FontStyle.Bold);
+            this.btn00.ForeColor = System.Drawing.Color.Maroon;
+            this.btn00.Location = new System.Drawing.Point(13, 13);
+            this.btn00.Name = "btn00";
+            this.btn00.Size = new System.Drawing.Size(100, 100);
+            this.btn00.TabIndex = 0;
+            this.btn00.UseVisualStyleBackColor = false;
+            this.btn00.Click += new System.EventHandler(this.btn00_Click);
+            // 
+            // btn01
+            // 
+            this.btn01.BackColor = System.Drawing.Color.White;
+            this.btn01.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn01.FlatAppearance.BorderSize = 0;
+            this.btn01.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn01.Font = new System.Drawing.Font("Arial", 40F, System.Drawing.FontStyle.Bold);
+            this.btn01.ForeColor = System.Drawing.Color.Maroon;
+            this.btn01.Location = new System.Drawing.Point(119, 12);
+            this.btn01.Name = "btn01";
+            this.btn01.Size = new System.Drawing.Size(100, 100);
+            this.btn01.TabIndex = 1;
+            this.btn01.UseVisualStyleBackColor = false;
+            this.btn01.Click += new System.EventHandler(this.btn01_Click);
+            // 
+            // btn02
+            // 
+            this.btn02.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn02.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn02.FlatAppearance.BorderSize = 0;
+            this.btn02.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn02.Font = new System.Drawing.Font("Arial", 40F, System.Drawing.FontStyle.Bold);
+            this.btn02.ForeColor = System.Drawing.Color.Maroon;
+            this.btn02.Location = new System.Drawing.Point(225, 13);
+            this.btn02.Name = "btn02";
+            this.btn02.Size = new System.Drawing.Size(100, 100);
+            this.btn02.TabIndex = 2;
+            this.btn02.UseVisualStyleBackColor = false;
+            this.btn02.Click += new System.EventHandler(this.btn02_Click);
+            // 
+            // btn12
+            // 
+            this.btn12.BackColor = System.Drawing.Color.White;
+            this.btn12.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn12.FlatAppearance.BorderSize = 0;
+            this.btn12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn12.Font = new System.Drawing.Font("Arial", 40F, System.Drawing.FontStyle.Bold);
+            this.btn12.ForeColor = System.Drawing.Color.Maroon;
+            this.btn12.Location = new System.Drawing.Point(225, 119);
+            this.btn12.Name = "btn12";
+            this.btn12.Size = new System.Drawing.Size(100, 100);
+            this.btn12.TabIndex = 3;
+            this.btn12.UseVisualStyleBackColor = false;
+            this.btn12.Click += new System.EventHandler(this.btn12_Click);
+            // 
+            // btn11
+            // 
+            this.btn11.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn11.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn11.FlatAppearance.BorderSize = 0;
+            this.btn11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn11.Font = new System.Drawing.Font("Arial", 40F, System.Drawing.FontStyle.Bold);
+            this.btn11.ForeColor = System.Drawing.Color.Maroon;
+            this.btn11.Location = new System.Drawing.Point(119, 119);
+            this.btn11.Name = "btn11";
+            this.btn11.Size = new System.Drawing.Size(100, 100);
+            this.btn11.TabIndex = 4;
+            this.btn11.UseVisualStyleBackColor = false;
+            this.btn11.Click += new System.EventHandler(this.btn11_Click);
+            // 
+            // btn10
+            // 
+            this.btn10.BackColor = System.Drawing.Color.White;
+            this.btn10.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn10.FlatAppearance.BorderSize = 0;
+            this.btn10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn10.Font = new System.Drawing.Font("Arial", 40F, System.Drawing.FontStyle.Bold);
+            this.btn10.ForeColor = System.Drawing.Color.Maroon;
+            this.btn10.Location = new System.Drawing.Point(13, 119);
+            this.btn10.Name = "btn10";
+            this.btn10.Size = new System.Drawing.Size(100, 100);
+            this.btn10.TabIndex = 5;
+            this.btn10.UseVisualStyleBackColor = false;
+            this.btn10.Click += new System.EventHandler(this.btn10_Click);
+            // 
+            // btn22
+            // 
+            this.btn22.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn22.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn22.FlatAppearance.BorderSize = 0;
+            this.btn22.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn22.Font = new System.Drawing.Font("Arial", 40F, System.Drawing.FontStyle.Bold);
+            this.btn22.ForeColor = System.Drawing.Color.Maroon;
+            this.btn22.Location = new System.Drawing.Point(225, 225);
+            this.btn22.Name = "btn22";
+            this.btn22.Size = new System.Drawing.Size(100, 100);
+            this.btn22.TabIndex = 6;
+            this.btn22.UseVisualStyleBackColor = false;
+            this.btn22.Click += new System.EventHandler(this.btn22_Click);
+            // 
+            // btn21
+            // 
+            this.btn21.BackColor = System.Drawing.Color.White;
+            this.btn21.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn21.FlatAppearance.BorderSize = 0;
+            this.btn21.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn21.Font = new System.Drawing.Font("Arial", 40F, System.Drawing.FontStyle.Bold);
+            this.btn21.ForeColor = System.Drawing.Color.Maroon;
+            this.btn21.Location = new System.Drawing.Point(118, 225);
+            this.btn21.Name = "btn21";
+            this.btn21.Size = new System.Drawing.Size(100, 100);
+            this.btn21.TabIndex = 7;
+            this.btn21.UseVisualStyleBackColor = false;
+            this.btn21.Click += new System.EventHandler(this.btn21_Click);
+            // 
+            // btn20
+            // 
+            this.btn20.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn20.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn20.FlatAppearance.BorderSize = 0;
+            this.btn20.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn20.Font = new System.Drawing.Font("Arial", 40F, System.Drawing.FontStyle.Bold);
+            this.btn20.ForeColor = System.Drawing.Color.Maroon;
+            this.btn20.Location = new System.Drawing.Point(12, 225);
+            this.btn20.Name = "btn20";
+            this.btn20.Size = new System.Drawing.Size(100, 100);
+            this.btn20.TabIndex = 8;
+            this.btn20.UseVisualStyleBackColor = false;
+            this.btn20.Click += new System.EventHandler(this.btn20_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::X_O_GAME.Properties.Resources._7rf_X_26;
+            this.pictureBox2.Location = new System.Drawing.Point(458, 38);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(276, 310);
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::X_O_GAME.Properties.Resources.ooo;
+            this.pictureBox1.Location = new System.Drawing.Point(458, 53);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(290, 295);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 413);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn20);
+            this.Controls.Add(this.btn21);
+            this.Controls.Add(this.btn22);
+            this.Controls.Add(this.btn10);
+            this.Controls.Add(this.btn11);
+            this.Controls.Add(this.btn12);
+            this.Controls.Add(this.btn02);
+            this.Controls.Add(this.btn01);
+            this.Controls.Add(this.btn00);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn00;
+        private System.Windows.Forms.Button btn01;
+        private System.Windows.Forms.Button btn02;
+        private System.Windows.Forms.Button btn12;
+        private System.Windows.Forms.Button btn11;
+        private System.Windows.Forms.Button btn10;
+        private System.Windows.Forms.Button btn22;
+        private System.Windows.Forms.Button btn21;
+        private System.Windows.Forms.Button btn20;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ X_O_GAME/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace X_O_GAME
+{
+    public partial class Form1 : Form
+    {
+        enum GameType { X, O } ;
+        GameType current_gt = GameType.X;
+        int played_games = 0;
+        Button[,] btns = new Button[3, 3]; 
+
+        public Form1()
+        {
+            InitializeComponent();
+            btns[0, 0] = btn00; btns[0, 1] = btn01; btns[0, 2] = btn02;
+            btns[1, 0] = btn10; btns[1, 1] = btn11; btns[1, 2] = btn12;
+            btns[2, 0] = btn20; btns[2, 1] = btn21; btns[2, 2] = btn22;
+            foreach (Button btn in btns) btn.Click += new EventHandler(btn_Click);  
+        }
+        void btn_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn.Text != "") return;
+            played_games++; 
+            btn.Text = current_gt.ToString();
+            if (Iswin())
+            {    MessageBox.Show(current_gt.ToString() + "  Is win");
+            Newgame();
+            return;
+            }
+             if (played_games == 9) 
+             {
+                 MessageBox.Show("The score is equal");
+                 Newgame(); 
+             }
+             Swich_game(); 
+        }
+       
+
+        void Newgame()
+        {
+            foreach (Button btn in btns) btn.Text = "";
+            played_games = 0;
+        }
+            void Swich_game()
+            {
+                if (current_gt == GameType.X)
+                    current_gt = GameType.O;
+                else
+                    current_gt = GameType.X; 
+            }
+
+            bool Iswin()
+            {
+                for (int i = 0; i <= 2; i++)
+                {
+                    if (btns[0, i].Text == current_gt.ToString() &&
+                        btns[0, i].Text == btns[1, i].Text &&
+                        btns[1, i].Text == btns[2, i].Text)
+                        return true;
+                    if (btns[i,0].Text == current_gt.ToString() &&
+                        btns[i,0].Text == btns[i,1].Text &&
+                        btns[i,1].Text == btns[i,2].Text)
+                        return true;    
+                }
+
+                if (btns[0, 0].Text == current_gt.ToString() &&
+                        btns[0, 0].Text == btns[1, 1].Text &&
+                        btns[1, 1].Text == btns[2, 2].Text)
+                    return true;
+
+                if (btns[0, 2].Text == current_gt.ToString() &&
+                        btns[0, 2].Text == btns[1, 1].Text &&
+                        btns[1,1].Text == btns[2, 0].Text)
+                    return true;
+                return false;
+
+               
+            }
+        
+
+            private void btn00_Click(object sender, EventArgs e)
+            {
+               if( pictureBox2.Visible == false)
+                pictureBox2.Visible = true;
+               else if (pictureBox2.Visible == true)
+                   pictureBox2.Visible = false;
+               
+            }
+
+            private void btn01_Click(object sender, EventArgs e)
+            {
+                if (pictureBox2.Visible == false)
+                    pictureBox2.Visible = true;
+                else if (pictureBox2.Visible == true)
+                    pictureBox2.Visible = false;
+                
+            }
+
+            private void btn02_Click(object sender, EventArgs e)
+            {
+                if (pictureBox2.Visible == false)
+                    pictureBox2.Visible = true;
+                else if (pictureBox2.Visible == true)
+                    pictureBox2.Visible = false;
+                
+            }
+
+            private void btn10_Click(object sender, EventArgs e)
+            {
+                if (pictureBox2.Visible == false)
+                    pictureBox2.Visible = true;
+                else if (pictureBox2.Visible == true)
+                    pictureBox2.Visible = false;
+                
+            }
+
+            private void btn11_Click(object sender, EventArgs e)
+            {
+                if (pictureBox2.Visible == false)
+                    pictureBox2.Visible = true;
+                else if (pictureBox2.Visible == true)
+                    pictureBox2.Visible = false;
+                
+            }
+
+            private void btn12_Click(object sender, EventArgs e)
+            {
+                if (pictureBox2.Visible == false)
+                    pictureBox2.Visible = true;
+                else if (pictureBox2.Visible == true)
+                    pictureBox2.Visible = false;
+                
+            }
+
+            private void btn20_Click(object sender, EventArgs e)
+            {
+                if (pictureBox2.Visible == false)
+                    pictureBox2.Visible = true;
+                else if (pictureBox2.Visible == true)
+                    pictureBox2.Visible = false;
+                
+            }
+
+            private void btn21_Click(object sender, EventArgs e)
+            {
+                if (pictureBox2.Visible == false)
+                    pictureBox2.Visible = true;
+                else if (pictureBox2.Visible == true)
+                    pictureBox2.Visible = false;
+                
+            }
+
+            private void btn22_Click(object sender, EventArgs e)
+            {
+                if (pictureBox2.Visible == false)
+                    pictureBox2.Visible = true;
+                else if (pictureBox2.Visible == true)
+                    pictureBox2.Visible = false;
+                
+            }
+       
+            
+            
+        
+    }
+}
